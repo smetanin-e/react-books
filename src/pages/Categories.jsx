c1e15ff795656fb4672ddc0321282449a5a6cfca
smetanin-e <e91smet15@gmail.com>
2025-05-17 10:37:19 +0300

    создание страницы категории

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
+import Aside from '../components/Aside'
+import Products from '../components/Products'
+
 function Categories() {
   return (
     <div className="wrapper">
@@ -10,7 +13,19 @@ function Categories() {
         
             <main className="page">
                 <div className="page__container">
-                   Категории
+                   <div class="page__products products-page">
+                    <Aside />
+
+                    <div class="products-page__books ">
+                        <div class="products-page__items">
+                                <h1 class="products-page__title">Категория</h1>
+                                <Products />
+
+                               
+                        </div>
+                    </div>
+
+                </div>
                 </div>
             </main>
 
