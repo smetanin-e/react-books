f6fee35a5ef957c4e77dde40dbfd56f91a23bd51
smetanin-e <e91smet15@gmail.com>
2025-05-18 18:34:33 +0300

    Установил Redux

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
+
 import './scss/style.scss';
 
 import App from './App';
+
+
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
