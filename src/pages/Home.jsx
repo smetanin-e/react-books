fecbc81ecdd4014f7f47a284ea7455c0f0ecd61c
smetanin-e <e91smet15@gmail.com>
2025-05-31 20:38:10 +0300

    Чистка кода

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,14 @@
-import React from 'react';
-import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { changeCategory } from '../redux/slices/categorySlice';
 import Banner from '../components/Banner';
 import Slider from '../components/Slider';
 import Tabs from '../components/Tabs';
 import Aside from '../components/Aside';
-import { useDispatch } from 'react-redux';
-import { changeCategory } from '../redux/slices/categorySlice';
 
 function Home() {
   const dispatch = useDispatch();
-
   //При переходе на главную страницу очищаем активную категорию
   dispatch(changeCategory(''));
-
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  //создаем состояние для хранения загруженных книг из БД
-  const [items, setItems] = React.useState([]);
-
-  //ссылка на массив объектов, содержащий книги
-  const url = 'https://815c3fb7d56c4537.mokky.dev/books';
-
-  //Оборачиваем запрос данных с сервера в useEffect, чтобы при каждом изменении
-  //не было нового рендера
-  React.useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await axios.get(url).then((response) => response.data);
-        setItems(response);
-        setIsLoading(false);
-        //Ищем книгу с максимальной скидкой и передаем ее в баннер
-      } catch (error) {
-        console.log(error.message || 'Произошла ошибка');
-      } finally {
-        window.scrollTo(0, 0);
-      }
-    };
-    getData();
-  }, []);
 
   return (
     <>
