764f8821c09e72dd88477f084771e282cd6a1b00
smetanin-e <e91smet15@gmail.com>
2025-05-31 20:02:12 +0300

    ///

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 export { default as Home} from './Home';
 export { default as BookPage} from './BookPage';
 export { default as Cart} from './Cart';
-export { default as Categories} from './Categories';
+export { default as Products} from './Products';
 export { default as NotFoundPage} from './NotFoundPage';
 export { default as Wish} from './Wish';
